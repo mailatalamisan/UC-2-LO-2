82705c0331d15b30c177ec13888032f583d6f7e1
mailatalamisan <76142191+mailatalamisan@users.noreply.github.com>
2021-01-08 16:33:10 +0800

    Add files via upload

---
+++ testifelse.js
@@ -0,0 +1,18 @@
+var myAge = 49;
+
+if (myAge > 46){
+
+    document.write("you are over 46!");
+
+} else if (myAge > 20){
+
+    document.write("you are over 20!");
+
+} else if (myAGe > 10){
+
+    document. write("you are over 10!");
+} else {
+
+    document.write("you are not over 10!");
+    
+}
---
+++ testoperators.js
@@ -0,0 +1,13 @@
+var myAge = 26;
+
+if (myAge < 18 || myAge > 30 || myAge === 25){
+
+    document.write("you aint coming!");
+    
+
+} else{
+    document.write ("You can come, you cool!");
+
+   
+
+}
